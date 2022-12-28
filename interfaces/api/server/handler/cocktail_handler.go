4f74d99b0777133d09ad8c6c6bdce9a816f68a4f
shake <hiroki.yamako@ezweb.ne.jp>
2022-12-28 17:31:57 +0900

    add cocktail create handler

--- interfaces/api/server/handler/cocktail_handler.go
+++ interfaces/api/server/handler/cocktail_handler.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/shake551/cocktails-api/application/usecase"
+	"github.com/shake551/cocktails-api/domain/model"
 	"log"
 	"net/http"
 	"strconv"
@@ -12,6 +14,7 @@ import (
 type CocktailHandler interface {
 	GetLimit(w http.ResponseWriter, r *http.Request)
 	GetById(w http.ResponseWriter, r *http.Request)
+	Create(w http.ResponseWriter, r *http.Request)
 }
 
 type cocktailHandler struct {
@@ -91,6 +94,66 @@ func (h *cocktailHandler) GetById(w http.ResponseWriter, r *http.Request) {
 	b, err := json.Marshal(cocktailsDetail)
 	if err != nil {
 		log.Printf("failed to parse json. err: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+type PostCocktailsBody struct {
+	Name      string                  `json:"name"`
+	Materials []PostCocktailsMaterial `json:"materials"`
+}
+
+type PostCocktailsMaterial struct {
+	Name     string                `json:"name"`
+	Quantity PostCocktailsQuantity `json:"quantity"`
+}
+
+type PostCocktailsQuantity struct {
+	Quantity json.Number `json:"quantity"`
+	Unit     string      `json:"unit"`
+}
+
+func (h *cocktailHandler) Create(w http.ResponseWriter, r *http.Request) {
+	body := &PostCocktailsBody{}
+	if err := json.NewDecoder(r.Body).Decode(body); err != nil {
+		log.Printf("bad request error. err: %v, body: %v", err, body)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	var materials []model.MaterialParams
+	for _, material := range body.Materials {
+		quantity, err := material.Quantity.Quantity.Int64()
+		if err != nil {
+			fmt.Println(err)
+		}
+		materials = append(materials, model.MaterialParams{Name: material.Name, Quantity: model.MaterialQuantity{
+			Quantity: quantity,
+			Unit:     material.Quantity.Unit,
+		}})
+	}
+
+	params := model.CocktailsParams{
+		Name:      body.Name,
+		Materials: materials,
+	}
+
+	coc, err := h.u.Create(r.Context(), params)
+	if err != nil {
+		log.Printf("failed to create cocktail. err: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(coc)
+	if err != nil {
+		log.Printf("failed to parse json")
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
