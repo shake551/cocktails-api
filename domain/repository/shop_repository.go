2086e3b25dd1bd681ee548d2615eab1c95322fe4
shake <hiroki.yamako@ezweb.ne.jp>
2022-12-29 12:06:03 +0900

    add shop add cocktail repository

--- domain/repository/shop_repository.go
+++ domain/repository/shop_repository.go
@@ -10,4 +10,5 @@ type ShopRepository interface {
 	Create(ctx context.Context, params model.ShopParams) (*model.Shop, error)
 	GetByID(ctx context.Context, id int64) (model.Shop, error)
 	GetShopCocktailList(ctx context.Context, shopID int64, limit int64, offset int64) ([]model.Cocktail, error)
+	AddShopCocktail(ctx context.Context, shopID int64, params model.ShopCocktailParams) ([]*model.ShopCocktail, error)
 }
