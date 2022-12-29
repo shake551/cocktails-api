0a3f106e5775535b3c7a8f9b4dc3492d5ba8c399
shake <hiroki.yamako@ezweb.ne.jp>
2022-12-29 10:37:18 +0900

    add shop create handler

--- interfaces/api/server/handler/shop_handler.go
+++ interfaces/api/server/handler/shop_handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"github.com/shake551/cocktails-api/application/usecase"
+	"github.com/shake551/cocktails-api/domain/model"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,6 +11,7 @@ import (
 
 type ShopHandler interface {
 	GetLimit(w http.ResponseWriter, r *http.Request)
+	Create(w http.ResponseWriter, r *http.Request)
 }
 
 type shopHandler struct {
@@ -60,6 +62,34 @@ func (h *shopHandler) GetLimit(w http.ResponseWriter, r *http.Request) {
 	b, err := json.Marshal(shops)
 	if err != nil {
 		log.Printf("failed to parse json. err: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func (h *shopHandler) Create(w http.ResponseWriter, r *http.Request) {
+	body := model.ShopParams{}
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		log.Printf("bad request error. err: %v, body:%v", err, body)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	s, err := h.u.Create(r.Context(), body)
+	if err != nil {
+		log.Printf("failed to create shop. err: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	b, err := json.Marshal(s)
+	if err != nil {
+		log.Printf("failed to parse json")
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
