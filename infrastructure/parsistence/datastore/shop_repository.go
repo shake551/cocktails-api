3f109c595deba24a2e009c7d7021197e57411377
shake <hiroki.yamako@ezweb.ne.jp>
2022-12-31 13:06:14 +0900

    add shop order provide repository

--- infrastructure/parsistence/datastore/shop_repository.go
+++ infrastructure/parsistence/datastore/shop_repository.go
@@ -398,3 +398,25 @@ func (r ShopRepository) Order(ctx context.Context, shopID int64, tableID int64, 
 
 	return orders, nil
 }
+
+func (r ShopRepository) OrderProvide(ctx context.Context, shopID int64, tableID int64, orderID int64) error {
+	log.Printf("order provide ... shopID: %d, tableID: %d, orderID: %d \n", shopID, tableID, orderID)
+
+	selectQuery := `SELECT 
+			shop_orders.*
+		FROM shop_tables
+			INNER JOIN shop_orders
+		WHERE shop_id=?
+			AND shop_tables.id=? 
+			AND shop_tables.id = shop_orders.table_id
+			AND shop_orders.id = ?`
+
+	_, err := db.DB.QueryContext(ctx, selectQuery, shopID, tableID, orderID)
+	if err != nil {
+		return err
+	}
+
+	q := `UPDATE shop_orders SET is_provided=true WHERE id=?`
+	_, err = db.DB.QueryContext(ctx, q, orderID)
+	return err
+}
