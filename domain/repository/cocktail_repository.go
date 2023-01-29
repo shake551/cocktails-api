a0d121d2ec2ebd4f9c448a5786702688da87f208
shake <hiroki.yamako@ezweb.ne.jp>
2023-01-29 17:45:24 +0900

    mock generate

--- domain/repository/cocktail_repository.go
+++ domain/repository/cocktail_repository.go
@@ -5,9 +5,11 @@ import (
 	"github.com/shake551/cocktails-api/domain/model"
 )
 
+//go:generate mockery --dir . --name CocktailRepository --outpkg repository_mock --output ../repository_mock --case underscore
 type CocktailRepository interface {
 	GetLimit(ctx context.Context, limit int64, offset int64, keyword string) ([]model.Cocktail, error)
 	GetByID(ctx context.Context, id int64) (model.CocktailDetail, error)
 	Create(ctx context.Context, params model.CocktailParams) (*model.CocktailDetail, error)
 	GetListByIDs(ctx context.Context, ids []int64) ([]model.Cocktail, error)
 }
+
